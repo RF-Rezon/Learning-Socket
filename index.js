075c8c4c66ec65c0507e8482b600d9bd906df040
Rezwan Faysal <rezwanfaysalrezon@gmail.com>
2024-03-16 13:56:02 +0600

    First push

--- index.js
+++ index.js
@@ -1,45 +1,34 @@
 const express = require("express");
 const app = express();
 
-// app.get("/", (req, res)=>{
-//     res.sendFile(__dirname+"/index.html")
-// });
-
-// app.listen("3000", ()=>{
-//    console.log("App is listening on port 3000")
-// })
-
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>        Another Approtch
-
-
 const http = require("http");
-const { Server } = require("socket.io");
+// const { Server } = require("socket.io");
 
 const expressServer = http.createServer(app);
 
-const io = new Server(expressServer);
+// const io = new Server(expressServer);
 
-const goLeft = io.of("/go_left");
-goLeft.on("connection", (socket) => {
-    console.log("New User Connected.");
+// const goLeft = io.of("/go_left");
+// goLeft.on("connection", (socket) => {
+//     console.log("New User Connected.");
     
-    goLeft.emit("namespace", "Go to left.")
+//     goLeft.emit("namespace", "Go to left.")
     
-    socket.on('disconnect', ()=>{
-        console.log("User Disconnected.")
-    })
-})
+//     socket.on('disconnect', ()=>{
+//         console.log("User Disconnected.")
+//     })
+// })
 
-const goRight = io.of("/go_right");
-goRight.on("connection", (socket) => {
-    console.log("New User Connected.");
+// const goRight = io.of("/go_right");
+// goRight.on("connection", (socket) => {
+//     console.log("New User Connected.");
 
-    goRight.emit("namespace", "Go to right.")
+//     goRight.emit("namespace", "Go to right.")
 
-    socket.on('disconnect', ()=>{
-        console.log("User Disconnected.")
-    })
-})
+//     socket.on('disconnect', ()=>{
+//         console.log("User Disconnected.")
+//     })
+// })
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")
